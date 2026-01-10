3dada928613f67e31dad7da7a3d814535b9d0096
Tharun <gowritharun461@gmail.com>
2026-01-10 18:34:51 +0530

    established the conection to database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import router from "./routes/memoRoute.js";
+import connectDB from "../config/db.js";
 
 const app = express();
 const PORT = 5050;
 
 app.use('/api/memories', router);
 
-app.listen(PORT, () => {
-    console.log(`Server waiting at http://localhost:${PORT}`);
-})
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server waiting at http://localhost:${PORT}`);
+    })
+});
+// mongodb+srv://gowritharun461_db_user:VLtD8NLi2Na7mPeg@cluster0.xphu4ut.mongodb.net/?appName=Cluster0
