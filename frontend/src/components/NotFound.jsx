47a599e1fb0058e1b9f956f5f20c47282174d5f7
Tharun <gowritharun461@gmail.com>
2026-01-30 12:52:08 +0530

    Re-add frontend Dir

---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+
+// create a NotFound component that displays a 404 error message
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>NotFound</div>
+  )
+}
+
+export default NotFound
