61366466e45ab58cadf485751248a1beff814741
tharun02 <gowritharun461@gmail.com>
2026-02-28 21:36:59 +0530

    Change API base URL to production server

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050/api/memories",
+  baseURL: "https://memory-archieve.onrender.com/api/memories",
   headers: {
     "Content-Type": "application/json",
   },
